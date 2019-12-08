df952f8439c0ae54a614d2813d859ee2e263f545
Tylere Couture <tylerecouture@gmail.com>
2019-12-07 17:54:17 -0800

    initial file transfer

---
+++ src/Actors/Player.gd
@@ -0,0 +1,50 @@
+extends Actor
+
+export var stomp_impulse: = 1000.0
+
+func _on_EnemyDetector_area_entered(area: Area2D) -> void:
+	print("stomping!")
+	_velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
+	
+	
+func _on_EnemyDetector_body_entered(body: PhysicsBody2D) -> void:
+	queue_free()
+
+func _physics_process(delta: float) -> void:
+	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
+	var direction: = get_direction()
+	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
+	_velocity = move_and_slide(_velocity, Vector2.UP)
+	
+func get_direction() -> Vector2:
+	return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
+	)
+	
+func calculate_move_velocity(
+		linear_velocity: Vector2,
+		direction: Vector2,
+		speed: Vector2,
+		is_jump_interrupted: bool
+	) -> Vector2:
+	var new_v: = linear_velocity
+	new_v.x = speed.x * direction.x
+	new_v.y += gravity * get_physics_process_delta_time()
+	if direction.y == -1.0:
+		new_v.y = speed.y * direction.y
+	if is_jump_interrupted:
+		new_v.y = 0
+	return new_v
+
+func calculate_stomp_velocity(
+		linear_velocity: Vector2, 
+		impulse: float
+	) -> Vector2:
+		var out: = linear_velocity
+		out.y = -impulse
+		return out
+		
+		
+
+
